5f5e6ff14d63066d9ca23e8a4271b02e2b9c93e2
Viktor_Bragin <vitya.bragin.1999@mail.ru>
2023-01-14 23:55:52 +0300

    Код к задаче на С# правка №1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите количество элементов массива: ");
 int size  = int.Parse(Console.ReadLine());
 
-string[] arr1 = new string[size];
+string[] array1 = new string[size];
 
 int count = 0;
 int maxSymbols = 3;
@@ -9,26 +9,26 @@ int maxSymbols = 3;
 for (int i = 0; i < size; i++){
 	Console.Write("Введите элемент массива: ");
     string result = Console.ReadLine();
-	arr1[i] = result;  
+	array1[i] = result;  
 }
 Console.WriteLine();
 Console.Write("Введенный массив: [");
-Console.Write(string.Join(",", arr1));
+Console.Write(string.Join(",", array1));
 Console.Write("]");
 
-for (int i = 0; i < arr1.Length; i++){
-	if(arr1[i].Length <= maxSymbols){
+for (int i = 0; i < array1.Length; i++){
+	if(array1[i].Length <= maxSymbols){
 		count++;
 	}   
 }
 Console.WriteLine();
 Console.Write("Полученный массив: [");
-string[] arr2 = new string[count];
+string[] array2 = new string[count];
 int j = 0;
-for (int i = 0; i < arr1.Length; i++){
-    if(arr1[i].Length <= maxSymbols){
-        arr2[j] = arr1[i];
-        Console.Write(arr2[j] + ",");
+for (int i = 0; i < array1.Length; i++){
+    if(array1[i].Length <= maxSymbols){
+        array2[j] = array1[i];
+        Console.Write(array2[j] + ",");
         j++;
     }
 }
